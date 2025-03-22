f3980a21927197e88c811e71be428cb572378ac2
Thaddeus <thaddeuspearson@gmail.com>
2025-03-22 23:31:02 +0100

    add ex_1 to Magic_Methods

---
+++ Object_Oriented_Programming_with_Python/Magic_Methods/ex_1.py
@@ -0,0 +1,57 @@
+"""
+Create a Car class that makes the following code work as indicated.
+"""
+
+
+class Car:
+
+    def __init__(self, name: str, year: int, color: str):
+        self.name = name
+        self.year = year
+        self.color = color
+
+    def __str__(self):
+        return f"{self.color.title()} {self.year} {self.name}"
+
+    def __repr__(self):
+        return f"Car({repr(self.name)}, {repr(self.year)}, {repr(self.color)})"
+
+    @classmethod
+    def _validate_input(cls, input: int | str,
+                        input_type: int | str,
+                        property: str):
+        if not isinstance(input, input_type):
+            raise TypeError(f"{property}: must be a {input_type}")
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        Car._validate_input(name, str, "name")
+        self._name = name
+
+    @property
+    def year(self):
+        return self._year
+
+    @year.setter
+    def year(self, year: int):
+        Car._validate_input(year, int, "year")
+        self._year = year
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, color: str):
+        Car._validate_input(color, str, "color")
+        self._color = color
+
+
+if __name__ == "__main__":
+    vwbuzz = Car('ID.Buzz', 2024, 'red')
+    assert str(vwbuzz) == "Red 2024 ID.Buzz", str(vwbuzz)
+    assert repr(vwbuzz) == "Car('ID.Buzz', 2024, 'red')", repr(vwbuzz)
