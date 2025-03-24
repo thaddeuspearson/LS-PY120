545983e3f39c9cbadc012f6bb7a2bf95fe50a7ce
Thaddeus <thaddeuspearson@gmail.com>
2025-03-24 22:38:13 +0100

    add generic-GREETING PART-2 to Classes_and_Objects_2

--- Object_Oriented_Programming/OO_Basics_Classes_and_Objects_2/generic_greeting.py
+++ Object_Oriented_Programming/OO_Basics_Classes_and_Objects_2/generic_greeting.py
@@ -8,9 +8,19 @@ from io import StringIO
 
 class Cat:
 
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
     @classmethod
     def generic_greeting(cls):
         print("Hello! I'm a cat!")
+
+    def personal_greeting(self):
+        print(f"Hello! My name is {self.name}!")
 
 
 if __name__ == "__main__":
@@ -18,5 +28,13 @@ if __name__ == "__main__":
     sys.stdout = StringIO()
     Cat.generic_greeting()
     output = sys.stdout.getvalue().strip()
+    assert str(output) == "Hello! I'm a cat!"
+
+    sys.stdout.seek(0)
+    sys.stdout.truncate(0)
+
+    kitty = Cat("Sophie")
+    kitty.personal_greeting()
+    output = sys.stdout.getvalue().strip()
     sys.stdout = original_stdout
-    assert output == "Hello! I'm a cat!"
+    assert output == "Hello! My name is Sophie!"
