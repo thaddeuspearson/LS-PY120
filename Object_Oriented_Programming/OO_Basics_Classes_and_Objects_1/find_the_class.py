2c5f4947cf6f50409227756a91721a6f238ed3b1
Thaddeus <thaddeuspearson@gmail.com>
2025-03-24 02:35:43 +0100

    add find_the_class to Objects_and_Classes_1

---
+++ Object_Oriented_Programming/OO_Basics_Classes_and_Objects_1/find_the_class.py
@@ -0,0 +1,8 @@
+"""
+Update the following code so that, instead of printing the values, each
+statement prints the name of the class to which it belongs.
+"""
+
+print(type("Hello"))        # <class 'str'>
+print(type(5))              # <class 'int'>
+print(type([1, 2, 3]))      # <class 'list'>
