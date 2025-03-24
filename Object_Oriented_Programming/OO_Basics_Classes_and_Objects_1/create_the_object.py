31069661f81be651a6a56ad132c45dfcdbf7def5
Thaddeus <thaddeuspearson@gmail.com>
2025-03-24 21:21:54 +0100

    add create_the_object to Classes_and_Objects_1

---
+++ Object_Oriented_Programming/OO_Basics_Classes_and_Objects_1/create_the_object.py
@@ -0,0 +1,15 @@
+"""
+Using the code from the previous exercise, create an instance of Cat and
+assign it to a variable named kitty.
+"""
+
+
+class Cat:
+    pass
+
+
+kitty = Cat()
+
+
+if __name__ == "__main__":
+    assert isinstance(kitty, Cat)
