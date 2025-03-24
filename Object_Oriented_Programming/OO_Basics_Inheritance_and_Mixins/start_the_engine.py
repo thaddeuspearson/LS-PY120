20a72f4c9c2a4ee4d18afde374d3337578f75e98
Thaddeus <thaddeuspearson@gmail.com>
2025-03-25 00:34:24 +0100

    add start_the_engine to Inheritance_and_Mixins

--- Object_Oriented_Programming/OO_Basics_Inheritance_and_Mixins/start_the_engine.py
+++ Object_Oriented_Programming/OO_Basics_Inheritance_and_Mixins/start_the_engine.py
@@ -1,6 +1,10 @@
 """
-Change the following code so that creating a new Truck automatically calls
-start_engine.
+Part One: Change the following code so that creating a new Truck automatically
+calls start_engine.
+
+Part Two: Given the following code, modify Truck.start_engine by appending
+'Drive fast, please!' to the return value of Vehicle.start_engine. The 'fast'
+in 'Drive fast, please!' should be taken from the value of the speed argument.
 """
 import sys
 from io import StringIO
@@ -8,21 +12,25 @@ from io import StringIO
 
 class Vehicle:
     def __init__(self, year: int):
-        self._year = year
+        self.year = year
 
     @property
     def year(self):
         return self._year
+
+    @year.setter
+    def year(self, year: int):
+        self._year = year
+
+    def start_engine(self, speed: str):
+        print(f'Ready to go! Drive {speed}, please!')
 
 
 class Truck(Vehicle):
 
     def __init__(self, year: int):
         super().__init__(year)
-        self.start_engine()
-
-    def start_engine(self):
-        print('Ready to go!')
+        self.start_engine("fast")
 
 
 if __name__ == "__main__":
@@ -31,5 +39,5 @@ if __name__ == "__main__":
     truck1 = Truck(1994)
     output = sys.stdout.getvalue().strip()
     sys.stdout = original_stdout
-    assert output == "Ready to go!"
+    assert output == "Ready to go! Drive fast, please!"
     assert truck1.year == 1994
