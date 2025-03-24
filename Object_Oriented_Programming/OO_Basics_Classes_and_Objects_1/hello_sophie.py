49d1b546a7bc881937b3c291779c712069fd264a
Thaddeus <thaddeuspearson@gmail.com>
2025-03-24 21:37:23 +0100

    add part 1 hello_sophie to Classes_and_Objects_1

---
+++ Object_Oriented_Programming/OO_Basics_Classes_and_Objects_1/hello_sophie.py
@@ -0,0 +1,32 @@
+"""
+Using the code from the previous exercise, add a parameter to __init__ that
+provides a name for the Cat object. Use an instance variable to print a
+greeting with the provided name.
+"""
+
+import sys
+from io import StringIO
+
+
+class Cat:
+
+    def __init__(self, name: str):
+        self.name = name
+        print(f"Hello! My name is {self.name}!")
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        self._name = name
+
+
+if __name__ == "__main__":
+    original_stdout = sys.stdout
+    sys.stdout = StringIO()
+    kitty = Cat("Sophie")
+    output = sys.stdout.getvalue().strip()
+    sys.stdout = original_stdout
+    assert str(output) == "Hello! My name is Sophie!"
