54b1ea4254925f583ee13cb1f646e2cdd03a5240
Thaddeus <thaddeuspearson@gmail.com>
2025-03-26 00:33:19 +0100

    add cats to Easy

---
+++ Object_Oriented_Programming/Easy/cats.py
@@ -0,0 +1,60 @@
+"""
+Update this code so you see the following output when you run the code:
+
+    My cat Cocoa is 3 years old and has black fur.
+    My cat Cheddar is 4 years old and has yellow and white fur.
+"""
+
+
+class Pet:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        self._name = name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age: str):
+        self._age = age
+
+
+class Cat(Pet):
+
+    def __init__(self, name: str, age: int, color: str):
+        super().__init__(name, age)
+        self.color = color
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, color: str):
+        self._color = color
+
+    @property
+    def info(self) -> str:
+        return str(
+            f"My {type(self).__name__.lower()} {self.name} is {self.age} years"
+            f" old and has {self.color} fur."
+        )
+
+
+if __name__ == "__main__":
+    cocoa = Cat('Cocoa', 3, 'black')
+    cheddar = Cat('Cheddar', 4, 'yellow and white')
+    print(cocoa.info)
+    assert cocoa.info == "My cat Cocoa is 3 years old and has black fur."
+    assert cheddar.info == (
+        "My cat Cheddar is 4 years old and has yellow and white fur."
+    )
