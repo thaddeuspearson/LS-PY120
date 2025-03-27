31a9a007ccff53a82d86ad2c3b523378fd53727a
Thaddeus <thaddeuspearson@gmail.com>
2025-03-27 01:38:10 +0100

    add wallet part_2 to Easy

--- Object_Oriented_Programming/Easy/wallet.py
+++ Object_Oriented_Programming/Easy/wallet.py
@@ -1,7 +1,12 @@
 """
+Part One:
 Implement a Wallet class that represents a wallet with a certain amount of
 money. You want to be able to combine (add) two wallets together to get a new
 wallet with the combined total amount from both wallets.
+
+Part Two:
+Modify the code so that when we print the merged_wallet we receive a message
+"Wallet with $80."
 """
 
 
@@ -18,9 +23,13 @@ class Wallet:
         self.amount += other
         return self
 
+    def __str__(self):
+        return f"Wallet with ${self.amount}"
+
 
 if __name__ == "__main__":
     wallet1 = Wallet(50)
     wallet2 = Wallet(30)
     merged_wallet = wallet1 + wallet2
     assert merged_wallet.amount == 80
+    assert str(merged_wallet) == f"Wallet with ${merged_wallet.amount}"
